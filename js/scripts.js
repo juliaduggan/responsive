8691abf6ad5850b1c81082c1e7fa8c83ed716f77
juliaduggan <123499191+juliaduggan@users.noreply.github.com>
2023-04-23 22:01:19 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,80 @@
+function showMenu() {
+    document.getElementById('navigation').style.transform = "translateX(0%)";
+    document.getElementById('x-icon').style.display = "block";
+    document.getElementById('hamburger-icon').style.display = "none";
+
+}
+function hideMenu() {
+    document.getElementById('navigation').style.transform = "translateX(100%)";
+    document.getElementById('x-icon').style.display = "none";
+    document.getElementById('hamburger-icon').style.display = "block";
+    // document.getElementById('navigation').style.display = "none";
+
+}
+
+window.addEventListener("resize", hideButtons);
+
+function hideButtons() {
+
+    if(window.innerWidth > 767) {
+        document.getElementById('x-icon').style.display = "none";
+        document.getElementById('hamburger-icon').style.display = "none";
+        document.getElementById('navigation').style.transform = "translateX(0%)";
+    }
+    else {
+        document.getElementById('hamburger-icon').style.display = "block";
+        document.getElementById('navigation').style.transform = "translateX(100%)";
+
+    }
+}
+
+function currentYear() {
+    let d = new Date();
+    document.getElementById('copyright').innerHTML = d.getFullYear();
+}
+
+let intIndex = 0
+
+function showImage(i) {
+    intIndex += i;
+
+    let images = document.getElementsByClassName('image');
+    let dot = document.getElementsByClassName('dot');
+
+    for (n = 0; n < images.length; n++) {
+        images [n].style.display = "none";
+        dot[n].className = dot[n].className.replace(" active", "");
+    }
+
+    if (intIndex > images.length -1) {
+        intIndex = 0;
+    }
+    if (intIndex < 0) {
+        intIndex = images.length -1;
+    }
+
+    images[intIndex].style.display = "block";
+    dot[intIndex].className += " active";
+}
+
+// When the user scrolls down 800px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        document.getElementById("cornerBtn").style.display = "block";
+    } else {
+        document.getElementById("cornerBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onload = function() {
+    currentYear();
+    showImage(intIndex);
+}
